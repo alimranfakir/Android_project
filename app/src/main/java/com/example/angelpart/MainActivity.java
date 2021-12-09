69d52092c5da732639b94b742449cfe28c22ff2f
AL IMRAN FAKIR <al.imran.fakir.cse@gmail.com>
2021-12-09 23:30:33 +0600

    first init

---
+++ app/src/main/java/com/example/angelpart/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.angelpart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import io.realm.Realm;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Realm.init(this);
+    }
+}
